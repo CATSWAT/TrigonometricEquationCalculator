f957f8044d8f9d6c239bd12130a2dfdf86d0a479
CATSWAT <ingushet00228@gmail.com>
2023-01-29 12:43:03 +0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TrigonometricEquationCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение угла в градусах: ");
+            double angle = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Значение синуса равно: {0}", Math.Sin(angle * Math.PI / 180));
+            Console.WriteLine("Значение косинуса равно: {0}", Math.Cos(angle * Math.PI / 180));
+            Console.WriteLine("Значение тангенса равно: {0}", Math.Tan(angle * Math.PI / 180));
+
+            Console.ReadKey();
+
+        }
+    }
+}
